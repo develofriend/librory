1456e8fa26f77032b338659a2843fa4367caa51e
dev friend <develofriend@gmail.com>
2018-01-29 12:34:41 +0800

    authors table seeder and factory
    Signed-off-by: dev friend <develofriend@gmail.com>

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
 
         $this->call(PublishersTableSeeder::class);
+        $this->call(AuthorsTableSeeder::class);
     }
 }
